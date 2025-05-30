b382eab8cde18fd2c1aff2993fdf044471641d4f
Erast_Pinskii <erast.ped@gmail.com>
2025-05-30 13:59:52 +0400

    improved templates
    improved all templates

--- tsp/settings.py
+++ tsp/settings.py
@@ -18,6 +18,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'tsp_database_app' / 'static',  # D:\tsp\tsp_database_app\static
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
